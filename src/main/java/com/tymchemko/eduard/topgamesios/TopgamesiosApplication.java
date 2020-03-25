6ab473a1e8f1f74f062ecbfe30dc32c49fa7491d
EduardTymchenko <timchenko_eduard@ukr.net>
2020-03-25 13:45:55 +0200

    added Front-end

--- src/main/java/com/tymchemko/eduard/topgamesios/TopgamesiosApplication.java
+++ src/main/java/com/tymchemko/eduard/topgamesios/TopgamesiosApplication.java
@@ -2,27 +2,30 @@ package com.tymchemko.eduard.topgamesios;
 
 import com.tymchemko.eduard.topgamesios.domain.Game;
 import com.tymchemko.eduard.topgamesios.domain.TypeGames;
-import com.tymchemko.eduard.topgamesios.service.GameService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 
 @SpringBootApplication
-public class TopgamesiosApplication  {
-    @Autowired
-    private GameService gameService;
+public class TopgamesiosApplication {
+    @Value("${spring.redis.host}")
+    private String hostRedis;
+    @Value("${spring.redis.port}")
+    private int portRedis;
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory(){
-        return new JedisConnectionFactory();
+    JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(hostRedis, portRedis);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
-    RedisTemplate<TypeGames, Game> redisTemplate(){
+    RedisTemplate<TypeGames, Game> redisTemplate() {
         RedisTemplate<TypeGames, Game> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(jedisConnectionFactory());
         return redisTemplate;
